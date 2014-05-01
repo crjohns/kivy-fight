35e5c97ec8d171d9f108d6c8cd752b0249ccfc41
Christopher R. Johnson <crjohns@csail.mit.edu>
2014-05-01 14:34:15 -0400

    now can do basic fight

--- main.py
+++ main.py
@@ -7,26 +7,47 @@ from random import randint
 from kivy.config import Config
 Config.set('graphics', 'width', '800')
 Config.set('graphics', 'height', '600')
+Config.set('graphics','resizable',0)
 
+
+game = None
 
 class Person:
+    name = ""
+    color = "ffffff"
     health = 100.0
 
     def isDead(self):
         return self.health < 1
 
-class Player(Person):
+    def hit_by(self, other):
+        val = randint(1,20)
+        self.health = max(0,self.health - val)
+        game.log("%s hit %s for %d\n%s" % (other.color_name(), self.color_name(), val, str(self)))
+
     def __str__(self):
-        return "[color=00ff00][YOU][/color] Health: " + str(int(self.health))
+        return "[color=%s][%s][/color] Health: %d" % (self.color, self.name, int(self.health))
+
+    def color_name(self):
+        return "[color=%s]%s[/color]" % (self.color, self.name)
+        
+
+class Player(Person):
+
+    def __init__(self):
+        self.name = "YOU"
+        self.color = "00ff00"
 
 class Enemy(Person):
-    def __str__(self):
-        return "[color=ff0000][ENEMY][/color] Health: " + str(int(self.health))
+    def __init__(self):
+        self.name = "ENEMY"
+        self.color = "ff0000"
 
 
 class FightGame(Widget):
 
     console = ObjectProperty(None)
+    attack = ObjectProperty(None)
 
     def update(self, delta):
         pass
@@ -36,9 +57,26 @@ class FightGame(Widget):
         for arg in args:
             self.console.text += str(arg)
 
+    def isGameOver(self):
+        return self.player.isDead() or self.enemy.isDead()
+
+    def press_attack(self, btn):
+        if self.isGameOver():
+            return
+
+        self.enemy.hit_by(self.player)
+        if self.enemy.isDead():
+            self.log("%s killed %s!" % (self.player.color_name(), self.enemy.color_name()))
+        else:
+            self.player.hit_by(self.enemy)
+
+        if self.player.isDead():
+            self.log("%s killed %s!" % (self.enemy.color_name(), self.player.color_name()))
+
 
 class FightApp(App):
     def build(self):
+        global game
         game = FightGame()
         game.player = Player()
         game.enemy = Enemy()
